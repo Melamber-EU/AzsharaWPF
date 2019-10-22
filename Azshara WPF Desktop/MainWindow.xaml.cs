fae43903a7abacf0bb73d8d3412953c593fe9164
griffr <griffr@gmail.com>
2019-10-22 12:24:45 +0100

    Add project files.

---
+++ Azshara WPF Desktop/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Azshara_WPF_Desktop.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Azshara_WPF_Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataTable dtTimers = new DataTable();
+        DataTable dtRaiders = new DataTable();
+        DataTable dtSetup = new DataTable();
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetupGrid();
+        }
+
+        private void SetupGrid()
+        {
+            
+            List<RaidersM> raidersData = RaidersModels.GetRaiders();
+            ObservableCollection<RaidersM> observableCollection = new ObservableCollection<RaidersM>();
+            //Bind the DataGrid to the customer data
+            dgvSoaks.DataContext = raidersData;
+        }
+    }
+}
